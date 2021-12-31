f2f95c1d62f8cc515b661f087347766b937a6082
Gendalf_Istari_2016 <palantir2018see@gmail.com>
2021-12-31 09:06:12 +0200

    2662

---
+++ Main.py
@@ -0,0 +1,6 @@
+import os,sys
+from HTTPRequst import HTTP, HTTP_Proxy, ProxyInfo, SelectTypeRequst, PROXY_Read_Write
+
+file="Test.txt"
+
+
--- Run_Install_Libs_86x.cmd
+++ Run_Install_Libs_86x.cmd
@@ -4,7 +4,7 @@ title Libs Install
 set file=requirements.txt
 echo FILE INSTALL: %file%
 title Libs Install %file%
-c:\Python38\Scripts\pip install -r %file%
+c:\Python38-32\Scripts\pip install -r %file%
 pause
 goto start
 pause
